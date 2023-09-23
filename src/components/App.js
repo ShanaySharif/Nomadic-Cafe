86de5dedce847b55e9b387798d83a86e65abe612
ShanaySharif <Shanay.uw17@gmail.com>
2023-09-23 12:33:56 -0400

    got page to finally render starting over

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,12 @@
 import React from "react";
 import Header from "./Header";
-import SignIn from "./SignIn";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import CafeControl from "./CafeControl";
 
-import CafeControl from "./CafeControl";
 function App(){
   return ( 
     <React.Fragment>
       <Header />
-      <CafeControl />
+      {/* <CafeControl /> */}
     </React.Fragment>
   );
 }
--- src/components/Cafe.js
+++ src/components/Cafe.js
@@ -1,46 +1,46 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
 
-function SignIn() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+// function SignIn() {
+//   const [email, setEmail] = useState('');
+//   const [password, setPassword] = useState('');
 
-  const handleEmailChange = (e) => {
-    setEmail(e.target.value);
-  };
+//   const handleEmailChange = (e) => {
+//     setEmail(e.target.value);
+//   };
 
-  const handlePasswordChange = (e) => {
-    setPassword(e.target.value);
-  };
+//   const handlePasswordChange = (e) => {
+//     setPassword(e.target.value);
+//   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // You can add your authentication logic here (e.g., send data to a server).
-  };
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//     // You can add your authentication logic here (e.g., send data to a server).
+//   };
 
-  return (
-    <div>
-      <h2>Sign In</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Email:</label>
-          <input
-            type="email"
-            value={email}
-            onChange={handleEmailChange}
-          />
-        </div>
-        <div>
-          <label>Password:</label>
-          <input
-            type="password"
-            value={password}
-            onChange={handlePasswordChange}
-          />
-        </div>
-        <button type="submit">Sign In</button>
-      </form>
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <h2>Sign In</h2>
+//       <form onSubmit={handleSubmit}>
+//         <div>
+//           <label>Email:</label>
+//           <input
+//             type="email"
+//             value={email}
+//             onChange={handleEmailChange}
+//           />
+//         </div>
+//         <div>
+//           <label>Password:</label>
+//           <input
+//             type="password"
+//             value={password}
+//             onChange={handlePasswordChange}
+//           />
+//         </div>
+//         <button type="submit">Sign In</button>
+//       </form>
+//     </div>
+//   );
+// }
 
-export default SignIn;
+// export default SignIn;
