ecf40730465e40b2daf80e6607ed614475b1ec4c
ShanaySharif <Shanay.uw17@gmail.com>
2023-09-23 17:18:48 -0400

    add button styling

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,20 @@
 import React from "react";
 import Header from "./Header";
+import Coffee from "./Coffee";
+
 // import CafeControl from "./CafeControl";
 
 
 
 function App() {
   return ( 
+    <>
     <Header />
+    <main className="main">
+      <Coffee />
+
+    </main>
+    </>
   );
 }
 
--- src/components/Coffee.js
+++ src/components/Coffee.js
@@ -2,12 +2,12 @@ const Coffee = () => {
     return <section
     className="hero section"
     id="home"
-    style={{backgroundImage: "url(./assets/media/bg-hero.jpg)"}}
+    style={{backgroundImage: "url(./media/bg-hero.jpg)"}}
   >
     <h1 className="hero__title">
       Start Your Day <br />
       With the best Coffee <br />
-      In Town.
+      In Town
     </h1>
     <button className="hero__button">Order Now</button>
   </section>
